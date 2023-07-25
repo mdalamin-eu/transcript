c08c1a3c975cb1dc49bb269686efc7d89aaca217
Md Alamin <58442420+mdalamin-eu@users.noreply.github.com>
2023-07-25 15:09:38 +0300

    Fix borders error

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-// // Get all certificate photos
-// const certificatePhotos = document.querySelectorAll('.certificate-container img');
 
-// // Add click event listener to each certificate photo
-// certificatePhotos.forEach(photo => {
-//   photo.addEventListener('click', (event) => {
-//     // Get the certificate photo's src and create a new img element
-//     const src = event.target.src;
-//     const newImg = document.createElement('img');
-//     newImg.src = src;
-
-//     // Create a div for the certificate photo and append the new img element
-//     const certificatePhotoDiv = document.createElement('div');
-//     certificatePhotoDiv.classList.add('certificate-photo');
-//     certificatePhotoDiv.appendChild(newImg);
-
-//     // Add click event listener to the certificate photo div to close it
-//     certificatePhotoDiv.addEventListener('click', () => {
-//       certificatePhotoDiv.remove();
-//     });
-
-//     // Append the certificate photo div to the body
-//     document.body.appendChild(certificatePhotoDiv);
-//   });
-// });
-
-// Get the modal
-// Get all the images with class="myImg" inside the "certificate-container" div
 var certificateImages = document.querySelectorAll(".certificate-container .certificate-img ");
 
 // Get the modal
